c4ec3dcf14ce5008fcf58a93d15c2d363f162755
hashHan <haeseong.han@gmail.com>
2018-03-17 11:10:42 +0900

    Added Singin
    next: move to store

--- myblog/src/routes.js
+++ myblog/src/routes.js
@@ -2,11 +2,13 @@ import Main from './components/Main.vue';
 import Posts from './components/sections/posts/Posts.vue';
 import PortfolioGrid from './components/sections/portfolios/PortfolioGrid.vue';
 import Signup from './components/auth/Signup.vue';
+import Signin from './components/auth/Signin.vue';
 
 export const routes = [
     { path: '/', name:'home', component: Main },
     { path: '/posts', name:'posts', component: Posts },
     { path: '/PortfolioGrid', name: 'portfolio', component: PortfolioGrid },
     { path: '/signup', name: 'signup', component: Signup },
+    { path: '/signin', name: 'signin', component: Signin },
     {path: '*', redirect: '/'}
 ];
